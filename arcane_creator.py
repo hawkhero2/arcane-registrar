58f25f589deb6e8f3dc62274764b60b4a49efddf
Ivanciu Andrei <47902631+hawkhero2@users.noreply.github.com>
2025-01-20 23:28:38 +0200

    Refactor and feature read users from file

--- arcane_creator.py
+++ arcane_creator.py
@@ -4,14 +4,15 @@ from lib.rocketchat_usr import create_rocketchat_user
 from lib.globals_vars import LOGFILE, LOGS_FORMAT
 import argparse
 import logging
+import os
 
 logger = logging.getLogger("Arcane Creator")
 parser = argparse.ArgumentParser(description="Utility script for creating users in multiple environments and performing ldapsearch")
 parser.add_argument("--username", help="Username to be created",default="")
 parser.add_argument("--fullname", help="Full name of the user",default="")
 parser.add_argument("--password", help="Password for the user",default="")
+parser.add_argument("--file", help="Provide file for creating multiple users; Must be in the format, each user on a new line: user,fullname,pass,email",default="")
 parser.add_argument("--email", help="Provide email for the user, needed for chat account", default="")
-# parser.add_argument("--check", help="Perform ldapsearch on ldap, provide y/n", choices=["y","n"], default="n")
 args = parser.parse_args()
 
 def main():
@@ -21,25 +22,18 @@ def main():
     print("Executing script...")
     print("------------------------------------------------")
 
-    create_ldap_user(username=args.username, fullname=args.fullname, password=args.password)
-    create_winsv_user(username=args.username, fullname=args.fullname, password=args.password) 
-    create_rocketchat_user(username=args.username, fullname=args.fullname, password=args.password, email=args.email) 
-
-    # if(args.checkRooms == "y"):
-    #     create_rocketchat_user(username="testuser125",fullname="Test name",password="Parola123$",email="testuser12@gmail.com")
-    # if(args.check == "y"):
-    #     ldap_search("objectclass=*") 
-    # else:
-    #     # Just for testing
-    #     if args.username == "" or args.fullname == "" or args.password == "":
-    #         print("Didnt' provide values for either --username, --fullname or password.\nUsing some values for testing...") 
-    #         usr ="gog13"
-    #         fn="gog k"
-    #         pw="Parola123$"
-    #         create_ldap_user(usr,fn,pw)
-
-    #     else:
-    #         create_ldap_user(args.username, args.fullname, args.password)
+    if args.file == "":
+        if args.username != "" and args.fullname != "" and args.password != "" and args.email !="":
+            create_ldap_user(username=args.username, fullname=args.fullname, password=args.password)
+            create_winsv_user(username=args.username, fullname=args.fullname, password=args.password) 
+            create_rocketchat_user(username=args.username, fullname=args.fullname, password=args.password, email=args.email) 
+    elif os.path.exists(args.file):
+        with open(args.file, "r") as f:
+            for line in f:
+                usr,fn,pw, mail = line.split(",")
+                create_ldap_user(username=usr, fullname=fn, password=pw)
+                create_winsv_user(username=usr, fullname=fn, password=pw)
+                create_rocketchat_user(username=usr, fullname=fn, password=pw, email=mail)
 
     logger.info("Finished running arcane creator")
 
